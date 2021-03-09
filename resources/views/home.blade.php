7a1ab89d231d11107162f598ca9782e16f882c03
Angel Perez <agperezb19@gmail.com>
2021-03-09 14:58:03 -0500

    diseño de home
    se diseño el 60% de home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,19 +14,167 @@
                 <li><a href="#ventajas" class="menu_items_list">Ventajas</a></li>
                 <li><a href="#planes" class="menu_items_list">Planes</a></li>
                 <li><a href="#contacto" class="menu_items_list">Contacto</a></li>
-                <li><a href=""class="menu_items_botton">INGRESAR</a></li>
-                <li><a href=""class="menu_items_bottom-bold">CREAR CUENTA</a></li>
+                <li><a href="" class="menu_items_botton">INGRESAR</a></li>
+                <li><a href="" class="menu_items_bottom-bold">CREAR CUENTA</a></li>
             </div>
         </nav>
         <section class="hero">
             <div class="hero-textos">
                 <h1>Software de Ventas <span>SOFTVENT</span> para Pymes</h1>
                 <p>Fácil de adquirir y usar, con un equipo de soporte listo cuando lo necesite!</p>
-                <a href="#" class="boton">CREAR CUENTA</a>
+                <div class="btn-group">
+                    <a href="#" class="boton">¡Contáctanos!</a>
+                    <a href="#" class="boton_bold">CREAR CUENTA</a>
+                </div>
             </div>
             <div class="hero-image">
-                <img src="" alt="">
+                <img src="{{asset('settings/img1.svg')}}" alt="reportes">
+            </div>
+            <div class="features">
+                <div class="feature-item">
+                    <img src="{{asset('settings/ordenador.svg')}}" alt="">
+                    <span>Inicie Fácil</span>
+                </div>
+                <div class="feature-item">
+                    <img src="{{asset('settings/config.svg')}}" alt="">
+                    <span>Agilice Procesos</span>
+                </div>
+                <div class="feature-item">
+                    <img src="{{asset('settings/negocio.svg')}}" alt="">
+                    <span>Controle su Empresa</span>
+                </div>
+                <div class="feature-item">
+                    <img src="{{asset('settings/investigacion.svg')}}" alt="">
+                    <span>Crezca sin Límites</span>
+                </div>
             </div>
         </section>
     </header>
+    <main>
+        <section class="containers" id="caracteristicas">
+            <h2 class="section-title">Siempre un paso adelante</h2>
+            <div class="advantage">
+                <div class="advantage-image">
+                    <img src="{{asset('settings/Analytics.svg')}}" alt="">
+                </div>
+                <div class="advantage-cards">
+                    <div class="item-one">
+                        <h2>SOFTVENT:La mejor opción para su éxito</h2>
+                        <span>No solo lo hacemos bien, también lo hacemos fácil</span>
+                    </div>
+                    <div class="card_item">
+                        <span><i class="fas fa-file-pdf"></i></span>
+                        <div class="card_item_text">
+                            <h2>Reportes PDF y Excel</h2>
+                            <p>SOFTVENT te permite generar reportes en PDF o Excel diarios, semanales o mensuales.</p>
+                        </div>
+                    </div>
+                    <div class="card_item">
+                        <span><i class="fas fa-file-alt"></i></span>
+                        <div class="card_item_text">
+                            <h2>Informacion 24/7</h2>
+                            <p> En SOFTVENT la informacion siempre estara disponible donde quiera que estes ya que se
+                                encuentra en servidores en la nume 24/7.</p>
+                        </div>
+                    </div>
+                    <div class="card_item">
+                        <span><i class="fas fa-plus"></i></span>
+                        <div class="card_item_text">
+                            <h2>Gana puntos</h2>
+                            <p>En SOFTVENT tus clientes podran acumular puntos que podran canjear en beneficios que tu
+                                dispongas.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="trust" id="ventajas">
+            <div class="trust-items">
+                <h2 class="section-title">Por qué confiar en SOFTVENT:</h2>
+                <div class="target">
+                    <div class="target-icon">
+                        <div class="content-target-icon">
+                            <i class="fas fa-users"></i>
+                        </div>
+                    </div>
+                    <div class="target-text">
+                        <h2>+1000</h2>
+                        <p>Usuarios</p>
+                    </div>
+                </div>
+                <div class="target target-one">
+                    <div class="target-icon">
+                        <div class="content-target-icon">
+                            <i class="fas fa-clipboard-list"></i>
+                        </div>
+                    </div>
+                    <div class="target-text">
+                        <h2>Ultimas tegnologías</h2>
+                        <p>Tegnologías actualizadas y seguras</p>
+                    </div>
+                </div>
+                <div class="target target-two">
+                    <div class="target-icon">
+                        <div class="content-target-icon">
+                            <i class="fas fa-clock"></i>
+                        </div>
+                    </div>
+                    <div class="target-text">
+                        <h2>+1 año</h2>
+                        <p>de experiencia en el mercado</p>
+                    </div>
+                </div>
+                <div class="target target-three">
+                    <div class="target-icon">
+                        <div class="content-target-icon">
+                            <i class="fas fa-medal"></i>
+                        </div>
+                    </div>
+                    <div class="target-text">
+                        <h2>4.7 estrellas</h2>
+                        <p>por nuestros clientes</p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="trust-image">
+                <img src="{{asset('settings/Ranking.svg')}}" alt="">
+            </div>
+        </section>
+        <section class="price" id="planes">
+            <h2 class="section-title">Planes</h2>
+            <div class="cards-content">
+                <div class="card-price">
+                    <h2>Plan Free</h2>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <a href=""></a>
+                </div>
+                <div class="card-price">
+                    <h2>Plan Mensual</h2>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <a href=""></a>
+                </div>
+                <div class="card-price">
+                    <h2>Plan Anual</h2>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <a href=""></a>
+                </div>
+            </div>
+        </section>
+    </main>
 @stop
